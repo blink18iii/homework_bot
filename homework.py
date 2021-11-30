f74954d77b19b97e2727a7bf0c8ddf7c64d4fa85
Max <blink18ii@gmail.com>
2021-11-30 12:34:30 +0100

    test

--- homework.py
+++ homework.py
@@ -9,8 +9,6 @@ import telegram
 from dotenv import load_dotenv
 
 load_dotenv()
-
-
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
